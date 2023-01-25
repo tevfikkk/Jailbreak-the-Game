0c6a3cf6298164ea4363e28703e0356a02822b84
Tevfik <79837579+tevfikkk@users.noreply.github.com>
2023-01-25 11:52:01 +0300

    char run

--- TileVania/Assets/Scripts/PlayerMovement.cs
+++ TileVania/Assets/Scripts/PlayerMovement.cs
@@ -6,22 +6,35 @@ using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
 
+    // Variables
+    [SerializeField] float moveSpeed = 3.5f;
+
+    // References 
     Vector2 moveInput;
+    Rigidbody2D myRb;
 
     void Start()
     {
-
+        myRb = GetComponent<Rigidbody2D>() as Rigidbody2D;
     }
 
 
     void Update()
     {
-
+        Run();
     }
 
+    // Input System
     void OnMove(InputValue context)
     {
         moveInput = context.Get<Vector2>();
         Debug.Log($"Move Input: {moveInput}");
+    }
+
+    // Run Method to move the player
+    void Run()
+    {
+        Vector2 playerVelocity = new Vector2(moveInput.x * moveSpeed, myRb.velocity.y);
+        myRb.velocity = playerVelocity;
     }
 }
