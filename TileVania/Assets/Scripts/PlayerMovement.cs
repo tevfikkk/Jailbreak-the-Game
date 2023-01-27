de4e014435dc7ef0dc17837f766191659c541a69
Tevfik <79837579+tevfikkk@users.noreply.github.com>
2023-01-27 13:09:10 +0300

    script

--- TileVania/Assets/Scripts/PlayerMovement.cs
+++ TileVania/Assets/Scripts/PlayerMovement.cs
@@ -10,65 +10,73 @@ public class PlayerMovement : MonoBehaviour
     // Variables
     [SerializeField] float moveSpeed = 3.5f;
     [SerializeField] float jumpSpeed = 15f;
+    float moveInput;
+    bool isFacingRight = true;
 
     // References 
-    Vector2 moveInput;
     Rigidbody2D myRb;
     Animator myAnim;
+    CapsuleCollider2D myCollider;
 
     void Start()
     {
         myRb = GetComponent<Rigidbody2D>() as Rigidbody2D;
         myAnim = GetComponent<Animator>() as Animator;
+        myCollider = GetComponent<CapsuleCollider2D>() as CapsuleCollider2D;
     }
 
 
     void Update()
     {
-        Run();
-        FlipPlayer();
-    }
-
-    // Input System
-    void OnMove(InputValue context)
-    {
-        moveInput = context.Get<Vector2>();
-        Debug.Log($"Move Input: {moveInput}");
-    }
-
-    void OnJump(InputValue context)
-    {
-        if (context.isPressed)
+        if (moveInput > 0f && !isFacingRight)
         {
-            Jump();
+            FlipPlayer();
+        }
+        else if (moveInput < 0f && isFacingRight)
+        {
+            FlipPlayer();
         }
     }
 
-    void Jump()
-    {
-        myRb.velocity = new Vector2(myRb.velocity.x, jumpSpeed);
+    void FixedUpdate() => Run();
 
+    // Input System
+    public void Move(InputAction.CallbackContext context)
+    {
+        moveInput = context.ReadValue<Vector2>().x;
+        Debug.Log($"Move Input: {moveInput}");
     }
+
+    public void Jump(InputAction.CallbackContext context)
+    {
+        if (context.performed && IsGrounded())
+        {
+            myRb.velocity = new Vector2(myRb.velocity.x, jumpSpeed);
+        }
+
+        if (context.canceled && myRb.velocity.y > 0)
+        {
+            myRb.velocity = new Vector2(myRb.velocity.x, myRb.velocity.y * 0.5f);
+        }
+    }
+
+    bool IsGrounded() => myCollider.IsTouchingLayers(LayerMask.GetMask("Ground"));
 
     // Flip Player
     void FlipPlayer()
     {
-        bool playerHasHorizontalSpeed = Mathf.Abs(myRb.velocity.x) > Mathf.Epsilon;
-        if (playerHasHorizontalSpeed)
-        {
-            transform.localScale = new Vector2(Mathf.Sign(myRb.velocity.x), 1f);
-        }
+        isFacingRight = !isFacingRight;
+        Vector3 localScale = transform.localScale;
+        localScale.x *= -1;
+        transform.localScale = localScale;
     }
 
-    // Run Method to move the player
     void Run()
     {
-        bool isRunning = Mathf.Abs(myRb.velocity.x) > Mathf.Epsilon;
+        bool playerHasHorizontalSpeed = Mathf.Abs(myRb.velocity.x) > Mathf.Epsilon;
 
-        Vector2 playerVelocity = new Vector2(moveInput.x * moveSpeed, myRb.velocity.y);
-        myRb.velocity = playerVelocity;
+        myAnim.SetBool("isRunning", playerHasHorizontalSpeed);
 
-        myAnim.SetBool("isRunning", isRunning);
-
+        myRb.velocity = new Vector2(moveInput * moveSpeed, myRb.velocity.y);
     }
 }
